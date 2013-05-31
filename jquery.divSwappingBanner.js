2b288d526387298c24a34764657a410e1ee9d987
Antony D'Andrea <contactme@antonydandrea.com>
2013-05-31 06:40:57 +0100

    initial commit

---
+++ jquery.divSwappingBanner.js
@@ -0,0 +1,59 @@
+(function($){
+    $.fn.divSwappingBanner = function(options) {
+
+        var defaults = {
+            image_height: "250px"
+        };
+        var options = $.extend(defaults, options);
+
+        var currentVisible, imageContainerWidth, imageContainerHeight;
+        var numberOfImages = 0;
+        $('#imageContainer').css('position', 'absolute');
+        $('#imageContainer').outerWidth("100%");
+        imageContainerWidth = $('#imageContainer').width();
+
+        $('img').each(function(){
+            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
+                numberOfImages = numberOfImages + 1;
+            }
+        });
+
+        $('img').each(function(){
+            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
+                $(this).css('-moz-box-sizing', 'border-box');
+                $(this).css('-webkit-box-sizing', 'border-box');
+                $(this).css('box-sizing', 'border-box');
+                $(this).css('float', 'left');
+
+                $(this).outerWidth(((100/numberOfImages))+"%");
+                $('#imageContainer').outerHeight(options['image_height']);
+
+                $(this).outerHeight("100%");
+
+                imageContainerHeight = $('#imageContainer').outerHeight();
+
+                $(this).click(function(){
+                    var id = $(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/);
+                    if (currentVisible !== "undefined" && currentVisible !== id[1]) {
+                        $("#divSwappingBanner-div-"+currentVisible).fadeOut();
+                    }
+                    $("#divSwappingBanner-div-"+id[1]).fadeIn();
+                    currentVisible = id[1];
+                });
+            }
+        });
+
+        $('div').each(function(){
+            if( $(this).attr('id').match(/divSwappingBanner\-div\-[0-9]{1}/) ) {
+                $(this).hide();
+                $(this).css('position', 'absolute');
+                $(this).css('-moz-box-sizing', 'border-box');
+                $(this).css('-webkit-box-sizing', 'border-box');
+                $(this).css('box-sizing', 'border-box');
+                $(this).css('margin', '0 auto');
+                $(this).outerWidth("100%");
+                $(this).css('margin-top', options['image_height']);
+            }
+        });
+    };
+})(jQuery);
