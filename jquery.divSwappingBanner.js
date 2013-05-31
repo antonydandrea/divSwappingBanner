e147fe164d888dfaba10b3b8cb790709ef8b6988
Antony D'Andrea <antony.dandrea@datatherapy.com>
2013-05-31 09:36:49 +0100

    Selected Image now highlights. Also an option to change selected image.

--- jquery.divSwappingBanner.js
+++ jquery.divSwappingBanner.js
@@ -4,6 +4,7 @@
         var defaults = {
             image_height: "250px",
             highlight_selected: true,
+            highlight_colour: "#FFFF85",
             default_image_selected: 1
         };
         var options = $.extend(defaults, options);
@@ -14,6 +15,86 @@
         $('#imageContainer').outerWidth("100%");
         imageContainerWidth = $('#imageContainer').width();
 
+        var unSelect = function(id) {
+            var el = "#divSwappingBanner-image-"+id;
+            var border_left_color  = $(el).data("border-left-color-old");
+            var border_right_color  = $(el).data("border-right-color-old");
+            var border_top_color = $(el).data("border-top-color-old");
+            var border_bottom_color  = $(el).data("border-bottom-color-old");
+
+            if (border_left_color === "none") {
+                $(el).css("border-left-style", "");
+            } else {
+                $(el).css("border-left-color", border_left_color);
+            }
+
+            if (border_right_color === "none") {
+                $(el).css("border-right-style", "");
+            } else {
+                $(el).css("border-right-color", border_right_color);
+            }
+
+            if (border_top_color === "none") {
+                $(el).css("border-top-style", "");
+            } else {
+                $(el).css("border-top-color", border_top_color);
+            }
+
+            if (border_bottom_color === "none") {
+                $(el).css("border-bottom-style", "");
+            } else {
+                $(el).css("border-bottom-color", border_bottom_color);
+            }
+
+        };
+
+        var showSelected = function(id) {
+            var el = "#divSwappingBanner-image-"+id;
+            var border_left_style  = $(el).css("border-left-style");
+            var border_right_style  = $(el).css("border-right-style");
+            var border_top_style  = $(el).css("border-top-style");
+            var border_bottom_style  = $(el).css("border-bottom-style");
+
+            if (border_left_style === "none") {
+                $(el).data("border-left-color-old", "none");
+                $(el).css("border-left-style", "solid");
+                $(el).css("border-left-color", options['highlight_colour']);
+                $(el).css("border-left-width", "2px");
+            } else {
+                $(el).data("border-left-color-old", $(el).css("border-left-color"));
+                $(el).css("border-left-color", options['highlight_colour']);
+            }
+
+            if (border_right_style === "none") {
+                $(el).data("border-right-color-old", "none");
+                $(el).css("border-right-style", "solid");
+                $(el).css("border-right-color", options['highlight_colour']);
+                $(el).css("border-right-width", "2px");
+            } else {
+                $(el).data("border-right-color-old", $(el).css("border-right-color"));
+                $(el).css("border-right-color", options['highlight_colour']);
+            }
+
+            if (border_top_style === "none") {
+                $(el).data("border-top-color-old", "none");
+                $(el).css("border-top-style", "solid");
+                $(el).css("border-top-color", options['highlight_colour']);
+                $(el).css("border-top-width", "2px");
+            } else {
+                $(el).data("border-top-color-old", $(el).css("border-top-color"));
+                $(el).css("border-top-color", options['highlight_colour']);
+            }
+
+            if (border_bottom_style === "none") {
+                $(el).data("border-bottom-color-old", "none");
+                $(el).css("border-bottom-style", "solid");
+                $(el).css("border-bottom-color", options['highlight_colour']);
+                $(el).css("border-bottom-width", "2px");
+            } else {
+                $(el).data("border-bottom-color-old", $(el).css("border-bottom-color"));
+                $(el).css("border-bottom-color", options['highlight_colour']);
+            }
+        };
 
         $('div').each(function(){
             if( $(this).attr('id').match(/divSwappingBanner\-div\-[0-9]{1}/) ) {
@@ -51,6 +132,7 @@
 
                 if (parseInt(options['default_image_selected']) !== 0) {
                     if (parseInt(id[1]) === parseInt(options['default_image_selected'])) {
+                        showSelected(id[1]);
                         $("#divSwappingBanner-div-"+id[1]).fadeIn();
                         currentVisible = id[1];
                     }
@@ -60,7 +142,8 @@
                     if (currentVisible !== "undefined" && currentVisible !== id[1]) {
                         $("#divSwappingBanner-div-"+currentVisible).fadeOut();
                     }
-
+                    unSelect(currentVisible);
+                    showSelected(id[1]);
                     $("#divSwappingBanner-div-"+id[1]).fadeIn();
                     currentVisible = id[1];
                 });
