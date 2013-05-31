407a46ad29e8aaf6102a1078577fe0c87f6067d3
Antony D'Andrea <antony.dandrea@datatherapy.com>
2013-05-31 09:00:46 +0100

    Have the ability to select an image that is selected when the page loads. This is by default the first image, selecting 0, turns it off and none of the images are loaded

--- jquery.divSwappingBanner.js
+++ jquery.divSwappingBanner.js
@@ -14,37 +14,6 @@
         $('#imageContainer').outerWidth("100%");
         imageContainerWidth = $('#imageContainer').width();
 
-        $('img').each(function(){
-            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
-                numberOfImages = numberOfImages + 1;
-            }
-        });
-
-        $('img').each(function(){
-            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
-                $(this).css('-moz-box-sizing', 'border-box');
-                $(this).css('-webkit-box-sizing', 'border-box');
-                $(this).css('box-sizing', 'border-box');
-                $(this).css('float', 'left');
-
-                $(this).outerWidth(((100/numberOfImages))+"%");
-                $('#imageContainer').outerHeight(options['image_height']);
-
-                $(this).outerHeight("100%");
-
-                imageContainerHeight = $('#imageContainer').outerHeight();
-
-                $(this).click(function(){
-                    var id = $(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/);
-                    if (currentVisible !== "undefined" && currentVisible !== id[1]) {
-                        $("#divSwappingBanner-div-"+currentVisible).fadeOut();
-                    }
-                    
-                    $("#divSwappingBanner-div-"+id[1]).fadeIn();
-                    currentVisible = id[1];
-                });
-            }
-        });
 
         $('div').each(function(){
             if( $(this).attr('id').match(/divSwappingBanner\-div\-[0-9]{1}/) ) {
@@ -58,5 +27,45 @@
                 $(this).css('margin-top', options['image_height']);
             }
         });
+
+        $('img').each(function(){
+            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
+                numberOfImages = numberOfImages + 1;
+            }
+        });
+
+        $('img').each(function(){
+            if($(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/)) {
+                var id = $(this).attr('id').match(/divSwappingBanner\-image\-([0-9]{1})/);
+                $(this).css('-moz-box-sizing', 'border-box');
+                $(this).css('-webkit-box-sizing', 'border-box');
+                $(this).css('box-sizing', 'border-box');
+                $(this).css('float', 'left');
+
+                $(this).outerWidth(((100/numberOfImages))+"%");
+                $('#imageContainer').outerHeight(options['image_height']);
+
+                $(this).outerHeight("100%");
+
+                imageContainerHeight = $('#imageContainer').outerHeight();
+
+                if (parseInt(options['default_image_selected']) !== 0) {
+                    if (parseInt(id[1]) === parseInt(options['default_image_selected'])) {
+                        $("#divSwappingBanner-div-"+id[1]).fadeIn();
+                        currentVisible = id[1];
+                    }
+                }
+
+                $(this).click(function(){
+                    if (currentVisible !== "undefined" && currentVisible !== id[1]) {
+                        $("#divSwappingBanner-div-"+currentVisible).fadeOut();
+                    }
+
+                    $("#divSwappingBanner-div-"+id[1]).fadeIn();
+                    currentVisible = id[1];
+                });
+            }
+        });
+
     };
 })(jQuery);
